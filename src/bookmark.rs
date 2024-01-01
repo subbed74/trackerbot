fb7e4dcf6a3c2866d0f403c15592b39e85ff1d79
subbed74 <subbed74@icloud.com>
2024-01-01 03:46:20 +0000

    Last bookmark commmand

--- src/bookmark.rs
+++ src/bookmark.rs
@@ -1,6 +1,7 @@
 use crate::{Context, Error};
-use crate::data::ServerBookmark;
+use crate::data::{TEAMMODES, ServerBookmark};
 use crate::admin::info_role;
+use crate::server::get_server_info;
 use serde_json::Value;
 
 /// Grab server information from a bookmark
@@ -14,7 +15,93 @@ pub async fn bk(
     ctx: Context<'_>,
     #[description = "Name of the server bookmark"] bookmark: String
 ) -> Result<(), Error> {
-    todo!()
+    let guild_id = *ctx.guild_id().unwrap().as_u64() as i64;
+
+    // Verify then grab information in DB
+    let count = sqlx::query!("SELECT COUNT(bookmark_name) AS count FROM server_bookmarks WHERE guild_id = ? AND bookmark_name = ?", guild_id, bookmark)
+        .fetch_one(&ctx.data().database)
+        .await
+        .unwrap();
+
+    if count.count == 0 {
+        return Err("No bookmark by that name exists!".into());
+    }
+
+    let bookmark_info = sqlx::query_as!(ServerBookmark, "SELECT * FROM server_bookmarks WHERE guild_id = ? AND bookmark_name = ?", guild_id, bookmark)
+        .fetch_one(&ctx.data().database)
+        .await
+        .unwrap();
+
+    let page_url = format!("https://sauertracker.net/server/{}/{}", bookmark_info.host, bookmark_info.port);
+
+    let server_data = match get_server_info(&ctx.data().client, bookmark_info.host.clone(), bookmark_info.port.clone()).await {
+        Ok(data) => data,
+        Err(e) => return Err(e)
+    };
+
+    // Format server info
+    let mut embed_desc = format!(
+        "**Players:** {}/{}\n**Mastermode:** {}\n*{} {} {}*\n\n",
+        server_data.clients,
+        server_data.maxClients,
+        server_data.masterMode,
+        server_data.mapName,
+        server_data.gameMode,
+        if server_data.gameMode != "coop_edit" {
+            format!("- {}", server_data.timeLeftString)
+        } else {
+            String::new()
+        }
+    );
+
+    ctx.send(|m| {
+        m.embed(|e| {
+            e.colour(0xFF0000);
+            e.title(server_data.description);
+            e.url(page_url);
+            e.description(embed_desc);
+
+            if TEAMMODES.contains(&server_data.gameMode.as_str()) {
+                for team in &server_data.teams {
+                    let mut team_players_display = String::new();
+                    for player in team.players.clone().unwrap() {
+                        team_players_display = format!("{}{}\n", team_players_display, player);
+                    }
+
+                    e.field(
+                        format!("{}: [{}]", team.name, team.score),
+                        team_players_display,
+                        true,
+                    );
+                }
+            } else {
+                let mut players_display = String::new();
+                for player in &server_data.all_active_players.unwrap() {
+                    players_display = format!("{}{}\n", players_display, player);
+                }
+
+                e.field("Players:", players_display, false);
+            }
+
+            if !server_data.spectators.clone().unwrap().is_empty() {
+                let mut spec_display = String::new();
+                for (i, player) in server_data.spectators.clone().unwrap().iter().enumerate() {
+                    if i == server_data.spectators.clone().unwrap().len() - 1 {
+                        spec_display = format!("{}{}", spec_display, player);
+                    } else {
+                        spec_display = format!("{}{}, ", spec_display, player);
+                    }
+                }
+
+                e.field("Spectators:", spec_display, false);
+            }
+
+            e.footer(|f| f.text(format!("/connect {} {}", bookmark_info.host, bookmark_info.port)))
+        })
+    })
+    .await?;
+
+    Ok(())
 }
 
 /// Create a server bookmark
